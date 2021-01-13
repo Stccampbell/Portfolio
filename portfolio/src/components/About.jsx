a72d8e7376040a50aad0fef70709ade66475a296
spencer campbell <stccampbell@gmail.com>
2021-01-13 12:05:05 -0600

    added the app and set up the out line page

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    
+
+    render() {
+        return(
+            <>
+            {/* <div style={{ backgroundImage: `url(${background})` }}>
+                Hello world
+            </div> */}
+            <h1>Spencer Campbell</h1>
+            <p> Lorem ipsum </p>
+            </>
+        )
+    }
+};
+
+export default Home;
